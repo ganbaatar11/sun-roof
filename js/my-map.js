6db57c9356d8e2c56b931ec2200112f2e892931b
ganbaatar11 <ganbaatar.gb@gmail.com>
2018-03-04 17:36:17 -0600

    getting area of shape

--- js/my-map.js
+++ js/my-map.js
@@ -4,54 +4,24 @@
 
 // initializing both searchbox and draw manager
 function initialize() {
-  
   initAutocomplete();
-  // initMap();
-}
-
-function initMap() {
-  var capital_city = {lat: 38.8937091, lng: -77.0846157}
-  var map = new google.maps.Map(document.getElementById('map'), {
-    center: capital_city,
-    zoom: 15,
-    mapTypeId: 'satellite',
-  });
-
-  var drawingManager = new google.maps.drawing.DrawingManager({
-    drawingMode: google.maps.drawing.OverlayType.MARKER,
-    drawingControl: true,
-    drawingControlOptions: {
-      position: google.maps.ControlPosition.TOP_CENTER,
-      drawingModes: ['polygon', 'rectangle']
-    },
-    markerOptions: {icon: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png'},
-    circleOptions: {
-      fillColor: '#ffff00',
-      fillOpacity: 1,
-      strokeWeight: 5,
-      clickable: false,
-      editable: true,
-      zIndex: 1
-    }
-  });
-  drawingManager.setMap(map);
 }
 
 function initAutocomplete() {
-  var capital_city = {lat: 38.8937091, lng: -77.0846157}
+  var chicago = {lat: 41.8851557, lng: -87.6292102}
   var map = new google.maps.Map(document.getElementById('map'), {
-    center: capital_city,
-    zoom: 13,
+    center: chicago,
+    zoom: 15,
     mapTypeId: 'satellite'
   });
-
+  
   // create drawing tools
   var drawingManager = new google.maps.drawing.DrawingManager({
-    drawingMode: google.maps.drawing.OverlayType.MARKER,
+    drawingMode: google.maps.drawing.OverlayType.OverlayType,
     drawingControl: true,
     drawingControlOptions: {
       position: google.maps.ControlPosition.TOP_CENTER,
-      drawingModes: ['polygon', 'rectangle']
+      drawingModes: ['polygon']
     },
     markerOptions: {icon: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png'},
     circleOptions: {
@@ -64,32 +34,45 @@ function initAutocomplete() {
     }
   });
 
+  // set map for drawing tools
+  drawingManager.setMap(map);
+
+  // adding overlaycomplete event listener on map
+  google.maps.event.addListener(drawingManager, 'overlaycomplete', function(event) {
+    if (event.type == 'polygon') {
+      var polygon = event.overlay.getPath();
+
+      // calculating area of shape in square meters.
+      var polygon_size = google.maps.geometry.spherical.computeArea(polygon);
+    }
+  });
+  
   // Create the search box and link it to the UI element.
   var input = document.getElementById('pac-input');
   var searchBox = new google.maps.places.SearchBox(input);
   map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
-
+  
   // Bias the SearchBox results towards current map's viewport.
   map.addListener('bounds_changed', function() {
     searchBox.setBounds(map.getBounds());
   });
-
+  
   var markers = [];
   // Listen for the event fired when the user selects a prediction and retrieve
   // more details for that place.
   searchBox.addListener('places_changed', function() {
     var places = searchBox.getPlaces();
-
+    
     if (places.length == 0) {
       return;
     }
-
+    
     // Clear out the old markers.
     markers.forEach(function(marker) {
       marker.setMap(null);
     });
     markers = [];
-
+    
     // For each place, get the icon, name and location.
     var bounds = new google.maps.LatLngBounds();
     places.forEach(function(place) {
@@ -104,7 +87,7 @@ function initAutocomplete() {
         anchor: new google.maps.Point(17, 34),
         scaledSize: new google.maps.Size(25, 25)
       };
-
+      
       // Create a marker for each place.
       markers.push(new google.maps.Marker({
         map: map,
@@ -112,7 +95,7 @@ function initAutocomplete() {
         title: place.name,
         position: place.geometry.location
       }));
-
+      
       if (place.geometry.viewport) {
         // Only geocodes have viewport.
         bounds.union(place.geometry.viewport);
@@ -120,10 +103,8 @@ function initAutocomplete() {
         bounds.extend(place.geometry.location);
       }
     });
-
+    
     // set map to fit bounds 
     map.fitBounds(bounds);
-    // set map for drawing tools
-    drawingManager.setMap(map);
   });
 }
