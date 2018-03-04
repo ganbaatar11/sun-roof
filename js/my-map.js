9304decd68397210330c846d7929f863fb75ebf7
ganbaatar11 <ganbaatar.gb@gmail.com>
2018-03-04 16:39:27 -0600

    set search box and drawing tool on map

--- js/my-map.js
+++ js/my-map.js
@@ -2,6 +2,13 @@
 // parameter when you first load the API. For example:
 // <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=drawing">
 
+// initializing both searchbox and draw manager
+function initialize() {
+  
+  initAutocomplete();
+  // initMap();
+}
+
 function initMap() {
   var capital_city = {lat: 38.8937091, lng: -77.0846157}
   var map = new google.maps.Map(document.getElementById('map'), {
@@ -28,4 +35,95 @@ function initMap() {
     }
   });
   drawingManager.setMap(map);
+}
+
+function initAutocomplete() {
+  var capital_city = {lat: 38.8937091, lng: -77.0846157}
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: capital_city,
+    zoom: 13,
+    mapTypeId: 'satellite'
+  });
+
+  // create drawing tools
+  var drawingManager = new google.maps.drawing.DrawingManager({
+    drawingMode: google.maps.drawing.OverlayType.MARKER,
+    drawingControl: true,
+    drawingControlOptions: {
+      position: google.maps.ControlPosition.TOP_CENTER,
+      drawingModes: ['polygon', 'rectangle']
+    },
+    markerOptions: {icon: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png'},
+    circleOptions: {
+      fillColor: '#ffff00',
+      fillOpacity: 1,
+      strokeWeight: 5,
+      clickable: false,
+      editable: true,
+      zIndex: 1
+    }
+  });
+
+  // Create the search box and link it to the UI element.
+  var input = document.getElementById('pac-input');
+  var searchBox = new google.maps.places.SearchBox(input);
+  map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+  // Bias the SearchBox results towards current map's viewport.
+  map.addListener('bounds_changed', function() {
+    searchBox.setBounds(map.getBounds());
+  });
+
+  var markers = [];
+  // Listen for the event fired when the user selects a prediction and retrieve
+  // more details for that place.
+  searchBox.addListener('places_changed', function() {
+    var places = searchBox.getPlaces();
+
+    if (places.length == 0) {
+      return;
+    }
+
+    // Clear out the old markers.
+    markers.forEach(function(marker) {
+      marker.setMap(null);
+    });
+    markers = [];
+
+    // For each place, get the icon, name and location.
+    var bounds = new google.maps.LatLngBounds();
+    places.forEach(function(place) {
+      if (!place.geometry) {
+        console.log("Returned place contains no geometry");
+        return;
+      }
+      var icon = {
+        url: place.icon,
+        size: new google.maps.Size(71, 71),
+        origin: new google.maps.Point(0, 0),
+        anchor: new google.maps.Point(17, 34),
+        scaledSize: new google.maps.Size(25, 25)
+      };
+
+      // Create a marker for each place.
+      markers.push(new google.maps.Marker({
+        map: map,
+        icon: icon,
+        title: place.name,
+        position: place.geometry.location
+      }));
+
+      if (place.geometry.viewport) {
+        // Only geocodes have viewport.
+        bounds.union(place.geometry.viewport);
+      } else {
+        bounds.extend(place.geometry.location);
+      }
+    });
+
+    // set map to fit bounds 
+    map.fitBounds(bounds);
+    // set map for drawing tools
+    drawingManager.setMap(map);
+  });
 }
